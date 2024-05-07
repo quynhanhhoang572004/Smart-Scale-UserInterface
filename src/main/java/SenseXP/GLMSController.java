2ce910f4377272e9130ab20b4e676d1b6126a923
quynhanhhoang572004 <quynhanhhoang572004@gmail.com>
2024-05-07 09:41:13 +0700

    Add controller for gmls, vas, timer

---
+++ src/main/java/SenseXP/GLMSController.java
@@ -0,0 +1,131 @@
+package SenseXP;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
+public class GLMSController {
+
+    @FXML
+    private Label BackGroundColorLabel;
+
+    @FXML
+    private StackPane GLMSStackPane;
+
+    @FXML
+    private AnchorPane Properties;
+
+    @FXML
+    private Button addConditionalStatementButton;
+
+    @FXML
+    private Button addCourseButton;
+
+    @FXML
+    private Button addFoodAndTasteButton;
+
+    @FXML
+    private Button addGLMSStageButton;
+
+    @FXML
+    private Button addInputButton;
+
+    @FXML
+    private Button addPeriodicStageButton;
+
+    @FXML
+    private Button addRatingsContainerButton;
+
+    @FXML
+    private Button addTasteTestButton;
+
+    @FXML
+    private Button addTimerButton;
+
+    @FXML
+    private Button addVASStageButton;
+
+    @FXML
+    private Button assignSoundButton;
+
+    @FXML
+    private Button audibleInstructionButton;
+
+    @FXML
+    private Label avatarLabel;
+
+    @FXML
+    private Button crossButton;
+
+    @FXML
+    private Button downButton;
+
+    @FXML
+    private Label labelEditExperiment;
+
+    @FXML
+    private Label labelSenseXP;
+
+    @FXML
+    private Button menuButton;
+
+    @FXML
+    private Button noticeStageButton;
+
+    @FXML
+    private Label textColorlabel;
+
+    @FXML
+    private TextArea textFrame;
+
+    @FXML
+    private TextArea timeField;
+
+    @FXML
+    private Button upButton;
+
+    @FXML
+    void AddGLMSbutton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void AddInputButton(ActionEvent event) throws IOException {
+        new ScenseSwitch(GLMSStackPane,"/fxml/InputStage.fxml");
+    }
+
+    @FXML
+    void AddNoticeButton(ActionEvent event) throws IOException {
+        new ScenseSwitch(GLMSStackPane,"/fxml/NoticeStage_1.fxml");
+
+    }
+
+    @FXML
+    void AddTimerButton(ActionEvent event) throws IOException {
+        new ScenseSwitch(GLMSStackPane,"/fxml/TimerStage.fxml");
+
+
+
+    }
+
+    @FXML
+    void AddVasButton(ActionEvent event) throws IOException {
+        new ScenseSwitch(GLMSStackPane,"/fxml/VasStage.fxml");
+
+
+    }
+
+    @FXML
+    void AssignSoundButton(ActionEvent event) throws IOException {
+        new ScenseSwitch(GLMSStackPane,"/fxml/AddSound.fxml");
+
+
+    }
+
+}
---
+++ src/main/java/SenseXP/VasController.java
@@ -0,0 +1,148 @@
+package SenseXP;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
+public class VasController {
+
+    @FXML
+    private Label PlaySoundLabel;
+
+    @FXML
+    private AnchorPane Properties;
+
+    @FXML
+    private StackPane VasStackPane;
+
+    @FXML
+    private Button addConditionalStatementButton;
+
+    @FXML
+    private Button addCourseButton;
+
+    @FXML
+    private Button addFoodAndTasteButton;
+
+    @FXML
+    private Button addGLMSStageButton;
+
+    @FXML
+    private Button addInputButton;
+
+    @FXML
+    private Button addPeriodicStageButton;
+
+    @FXML
+    private Button addRatingsContainerButton;
+
+    @FXML
+    private Button addTasteTestButton;
+
+    @FXML
+    private Button addTimerButton;
+
+    @FXML
+    private Button addVASStageButton;
+
+    @FXML
+    private Button assignSoundButton;
+
+    @FXML
+    private Button audibleInstructionButton;
+
+    @FXML
+    private Label avatarLabel;
+
+    @FXML
+    private TextArea buttonTxtField;
+
+    @FXML
+    private CheckBox checkBoxSwap;
+
+    @FXML
+    private ChoiceBox<?> choiceBoxAvail;
+
+    @FXML
+    private Button crossButton;
+
+    @FXML
+    private Button downButton;
+
+    @FXML
+    private TextArea highAncTxtField;
+
+    @FXML
+    private TextArea highAncValField;
+
+    @FXML
+    private Label labelEditExperiment;
+
+    @FXML
+    private Label labelSenseXP;
+
+    @FXML
+    private TextArea lowAncTxtField;
+
+    @FXML
+    private TextArea lowAncValField;
+
+    @FXML
+    private Button menuButton;
+
+    @FXML
+    private Button noticeStageButton;
+
+    @FXML
+    private TextArea questionField;
+
+    @FXML
+    private RadioButton radioButtonAvailable;
+
+    @FXML
+    private RadioButton radioButtonYes;
+
+    @FXML
+    private TextArea textFrame;
+
+    @FXML
+    private Button upButton;
+
+    @FXML
+    private TextArea yesField;
+
+    @FXML
+    void AddGLMSButton(ActionEvent event) throws IOException {
+        new ScenseSwitch(VasStackPane,"/fxml/GLMS.fxml");
+    }
+
+    @FXML
+    void AddInputStage(ActionEvent event) throws IOException {
+        new ScenseSwitch(VasStackPane,"/fxml/InputStage.fxml");
+    }
+
+    @FXML
+    void AddNoticeButton(ActionEvent event) throws IOException {
+        new ScenseSwitch(VasStackPane,"/fxml/NoticeStage_1.fxml");
+
+
+    }
+
+    @FXML
+    void AddTimerButton(ActionEvent event) throws IOException {
+        new ScenseSwitch(VasStackPane,"/fxml/TimerStage.fxml");
+
+
+
+    }
+
+}
